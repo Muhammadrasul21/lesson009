11a49993535305b2bca6abbb6d684ce4615b1f82
Muhammadrasul21 <theboy6878@gmail.com>
2024-09-15 00:27:36 +0500

    edited js

---
+++ app.js
@@ -0,0 +1,24 @@
+// {
+//     // 1 masala 
+
+//     class Kata {
+//         static getVolumeOfCuboid(length, width, height) {
+//             return length * width * height;
+//         }
+//     }
+
+// }
+
+
+
+
+
+// {
+//     // 2 masala
+
+//     function mango(quantity, price) {
+//         const paidMangoes = quantity - Math.floor(quantity / 3);
+//         return paidMangoes * price;
+//     }
+
+// }
