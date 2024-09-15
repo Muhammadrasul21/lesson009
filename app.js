20259b9fe26b9e25c62a9cadf5ab82459ff65248
Muhammadrasul21 <theboy6878@gmail.com>
2024-09-15 13:33:01 +0500

    edited js

--- app.js
+++ app.js
@@ -22,3 +22,94 @@
 //     }
 
 // }
+
+
+
+
+// {
+//     // 3 masala
+
+//     function basicOp(operation, value1, value2) {
+//         return operation === '+' ? value1 + value2 :
+//             operation === '-' ? value1 - value2 :
+//                 operation === '*' ? value1 * value2 :
+//                     operation === '/' ? value1 / value2 :
+//                         null;
+//     }
+// }
+
+
+
+
+
+// {
+//     // 4 masala 
+
+//     function arrayPlusArray(arr1, arr2) {
+//         let sum1 = arr1.reduce((acc, num) => acc + num, 0);
+//         let sum2 = arr2.reduce((acc, num) => acc + num, 0);
+//         return sum1 + sum2;
+//     }
+// }
+
+
+
+
+// {
+//     // 5 masala 
+
+//     function divisibleBy(numbers, divisor) {
+//         return numbers.filter(number => number % divisor === 0);
+
+//     }
+// }
+
+
+
+
+
+// {
+//     // 6 masala 
+
+//     function stringClean(s) {
+//         return s.replace(/[0-9]/g, '');
+//     }
+// }
+
+
+
+
+// {
+//     // 7 masala 
+
+//     function _if(bool, func1, func2) {
+//         bool ? func1() : func2();
+//     }
+// }
+
+
+
+// {
+//     // 8 masala 
+
+//     function whatday(num) {
+//         switch (num) {
+//             case 1:
+//                 return "Sunday";
+//             case 2:
+//                 return "Monday";
+//             case 3:
+//                 return "Tuesday";
+//             case 4:
+//                 return "Wednesday";
+//             case 5:
+//                 return "Thursday";
+//             case 6:
+//                 return "Friday";
+//             case 7:
+//                 return "Saturday";
+//             default:
+//                 return "Enter a number between 1 and 7";
+//         }
+//     }
+// }
